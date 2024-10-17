dce1203e570b4675bffdcacff95ba6b07a6843ab
Anup Shetty <anupshetty33@gmail.com>
2024-10-17 12:54:41 +0530

    av-2 : fix template parser deserialization

--- Uitlities/TemplateParser.cs
+++ Uitlities/TemplateParser.cs
@@ -1,11 +1,15 @@
+
+using System.Dynamic;
 using HandlebarsDotNet;
+using Newtonsoft.Json;
 
 public static class TemplateParser
 {
     public static string ParseEmailTemplate(string template, object model)
     {
         var compiledTemplate = Handlebars.Compile(template);
-        string emailContent = compiledTemplate(model);
+        var data = JsonConvert.DeserializeObject<dynamic>(model.ToString());
+        string emailContent = compiledTemplate(data);
         return emailContent;
     }
 }
